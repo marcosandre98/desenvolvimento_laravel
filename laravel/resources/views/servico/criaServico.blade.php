fa295a6a3e77c42f284ee74ed314238118b97512
eduardovilke <eduardo.vilke@unochapeco.edu.br>
2018-06-26 21:53:51 -0300

    envio de teste

--- laravel/resources/views/servico/criaServico.blade.php
+++ laravel/resources/views/servico/criaServico.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Adicionar Serviço</div>
+                <div class="panel-heading">Adicionar Servico</div>
                 <div class="panel-body">
 
                     <form class="form-horizontal" id="fCriaServico" data-route="{{route('servico.store')}}" data-method="POST">
